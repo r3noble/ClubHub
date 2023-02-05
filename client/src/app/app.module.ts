7f3a59c221f1008cdbf763e1785ce2d978e2c5a0
Jenna Sheldon <jennasheldon@ufl.edu>
2023-02-05 16:32:14 -0500

    lots of edits

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms'
-import {HttpClientModule} from '@angular/common/http'
-
+import { FormsModule } from '@angular/forms' ;
+import {HttpClientModule} from '@angular/common/http' ;
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    AppRoutingModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
