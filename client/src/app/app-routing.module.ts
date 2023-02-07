3982cbf8206b073de4669253eaa3ba7380131f09
r3noble <92817123+r3noble@users.noreply.github.com>
2023-02-07 18:45:50 -0500

    Login framework

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './home/home.component';
 import { AppComponent } from './app.component';
 import {Routes } from '@angular/router';
 import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './login/login.component';
 
 
 const appRoutes: Routes = [
@@ -15,6 +16,10 @@ const appRoutes: Routes = [
   {
     path: 'profile',
     component: ProfileComponent
+  }
+  {
+    path: 'login',
+    component: LoginComponent
   }
 ];
 
