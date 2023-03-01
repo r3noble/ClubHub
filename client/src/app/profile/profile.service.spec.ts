58b01e179ac1d6efc9a8a24cdfe85d9ab2bcef68
Jenna Sheldon <jennasheldon@ufl.edu>
2023-03-01 10:39:44 -0500

    removed dataserv changed to profileserv

--- client/src/app/profile/profile.service.spec.ts
+++ client/src/app/profile/profile.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { DataService } from './data.service';
+import { ProfileService } from './profile.service';
 
-describe('DataService', () => {
-  let service: DataService;
+describe('ProfileService', () => {
+  let service: ProfileService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(DataService);
+    service = TestBed.inject(ProfileService);
   });
 
   it('should be created', () => {
