e6aa547a4410a9ae232d6f651cd2a140c6d25dbe
Jenna Sheldon <jennasheldon@ufl.edu>
2023-04-14 14:01:17 -0400

    sure?!

--- client/src/bapp/api_clubhandlers.go
+++ client/src/bapp/api_clubhandlers.go
@@ -39,12 +39,38 @@ func (a *App) AddClubHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(newClub)
 }
-func (a *App) GetAllClubs(w http.ResponseWriter, r *http.Request){
+func (a *App) GetClubHandler(w http.ResponseWriter, r *http.Request){
 	//Get all clubs from the clubs database to check if exist
+	//var tmp models.Club
+	vars := mux.Vars(r)
+	name := vars["id"]
+	
+	club := models.Club{}
+	if err := a.Cdb.First(&club, models.Club{Name: name}).Error; err != nil {
+		fmt.Println("Club not located, adding to database...")
+		return
+	}
+	jsonResponse, err := json.Marshal(club)
+	if err != nil{
+		http.Error(w,err.Error(),http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonResponse)
+	return
 }
 
-func (a *App) JoinClubHandler(w http.ResponseWriter, r *http.Request) {
+/*func (a *App) JoinClubHandler(w http.ResponseWriter, r *http.Request) {
 	//get user from database
-	//determine if they are in the club
-	//
-}
+	var user model.User
+	err := json.NewDecoder(r.Body).Decode(&newUser)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+	//determine if they are in the club already
+		//if yes -> send back message to frontend to display error message
+		//otherwise-> edit the club column for their userDB slot
+			//must append a , and the club name to existing club string
+	//send back success message to front end
+}*/
