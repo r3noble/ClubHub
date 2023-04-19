d5ee33802e4c1e508715753689788d106b173c06
Jenna Sheldon <jennasheldon@ufl.edu>
2023-04-19 12:51:45 -0400

    events work w backend!

--- client/src/app/profile/profile.component.ts
+++ client/src/app/profile/profile.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AuthService } from '../login/auth.service';
 import { User } from '../user.model';
-
+import { CprofileService } from '../cprofile/cprofile.service';
+import { Club } from '../club.model';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
@@ -12,8 +13,10 @@ export class ProfileComponent implements OnInit {
   name: string  = "Please Login";
   email: string  = "";
   clubs: string = "No clubs joined yet!"
+  club:Club | null = null;
 
-  constructor(private route: ActivatedRoute, private authService: AuthService) {}
+
+  constructor(private route: ActivatedRoute, private authService: AuthService, private cprofile: CprofileService) {}
 
   ngOnInit() {
     const user = this.authService.getUser();
@@ -24,6 +27,16 @@ export class ProfileComponent implements OnInit {
       if ( this.clubs == "" || this.clubs == "No clubs joined!" ) {
         this.clubs = "No clubs joined!"
       }
+
+      this.cprofile.getClubInfo(user?.clubs as string).subscribe(
+        (club: Club) => {
+          this.club = club;
+          //console.log(this.club.VP);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
       //alert(this.clubs);
 
      // this.name = "not workin";
